d6e02a79694e1b0a6e53062ff83f9af811beaf42
ruth-magalhaes <ruth.magalhaes@sptech.school>
2025-04-12 17:14:36 -0300

    REQ-023 Criando crud address

---
+++ src/main/java/com/campestre/clube/backend_application/controller/mapper/AddressController.java
@@ -0,0 +1,4 @@
+package com.campestre.clube.backend_application.controller.mapper;
+
+public class AddressController {
+}
---
+++ src/main/java/com/campestre/clube/backend_application/service/AddressService.java
@@ -0,0 +1,11 @@
+package com.campestre.clube.backend_application.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressService {
+    @Autowired
+    // @Autowired
+    //    private AccountRepository accountRepository;
+}
